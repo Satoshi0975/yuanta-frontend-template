7f3f9d55988590f4263e966baff267034521c0a3
z01241 <z01241@lawbroker.com.tw>
2025-09-21 20:04:11 +0800

    feat: 新增滾動動畫

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,10 +31,13 @@ export default function Home() {
         <FixedBg>
           <SkyScroll />
         </FixedBg>
-        <div className="relative z-20">
+        <div className="relative z-20" id="registration">
           <HeroSection />
           <WayScroll />
-          <div className="container mx-auto mb-10 mt-10 space-y-10 px-4">
+          <div
+            className="container mx-auto mb-10 mt-10 space-y-10 px-4"
+            id="competition"
+          >
             <div className="grid grid-cols-1 items-stretch gap-4 md:grid-cols-2">
               <NewbieVillageSection />
               <DailyMissionSection />
@@ -42,8 +45,13 @@ export default function Home() {
             <MainMissionSection />
             <PopularityRankingSection />
           </div>
-          <VoteSection />
-          <div className="container -mt-2 flex flex-col px-4">
+          <div id="vote">
+            <VoteSection />
+          </div>
+          <div
+            className="container -mt-2 flex flex-col px-4 lg:mt-10"
+            id="hot-to"
+          >
             <SectionCard title="立即下單 賺積分">
               <div className="mx-auto max-w-[840px] space-y-4">
                 <Step />
