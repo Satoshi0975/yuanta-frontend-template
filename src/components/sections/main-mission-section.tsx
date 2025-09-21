60946c37f31c8a5712e976ae81cdff02a84f3dcb
z01241 <z01241@lawbroker.com.tw>
2025-09-21 20:46:43 +0800

    fix: 奇怪的錯誤

--- src/components/sections/main-mission-section.tsx
+++ src/components/sections/main-mission-section.tsx
@@ -61,6 +61,44 @@ export function MainMissionSection() {
             }
           />
 
+          {/* <div className="rounded-lg bg-black text-center text-xl shadow-lg nes-corners">
+            <div className="grid grid-cols-2 gap-1 md:grid-cols-[minmax(0,3fr)_minmax(0,3fr)_minmax(0,5fr)]">
+              <div className="bg-blue-600 p-3 font-semibold text-white">
+                TOP 10
+              </div>
+              <div className="bg-blue-400 p-3 font-semibold text-white">
+                獎金
+              </div>
+              <div className="hidden bg-blue-600 p-3 font-semibold text-white md:block">
+                獎品
+              </div>
+
+              <div className="bg-white p-3">第一名</div>
+              <div className="bg-white p-3">100,000</div>
+
+              <div className="bg-white p-3">第二名</div>
+              <div className="bg-white p-3">90,000</div>
+
+              <div className="bg-white p-3">第三名</div>
+              <div className="bg-white p-3">80,000</div>
+
+              <div className="bg-white p-3">第四名</div>
+              <div className="bg-white p-3">70,000</div>
+
+              <div className="bg-white p-3">第五名</div>
+              <div className="bg-white p-3">60,000</div>
+
+              <div className="col-span-2 bg-blue-600 p-3 font-semibold text-white md:col-span-1 md:col-start-3 md:row-span-5 md:row-start-2 md:hidden">
+                標題 3
+              </div>
+
+              <div className="col-span-2 bg-white p-4 md:col-span-1 md:col-start-3 md:row-span-5 md:row-start-2">
+                <div className="flex h-full flex-col justify-center">
+                </div>
+              </div>
+            </div>
+          </div> */}
+
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
             <PrizeTable title="TOP 3" titleColor="green" data={leftTableData} />
             <PrizeTable
