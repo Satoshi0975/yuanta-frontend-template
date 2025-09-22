d99e29674ec059eb5fd629606fb622e40f630d88
z01241 <z01241@lawbroker.com.tw>
2025-09-22 15:43:09 +0800

    feat: 調整河道特效會卡在圖片邊邊的問題

--- src/components/sections/way-scroll.tsx
+++ src/components/sections/way-scroll.tsx
@@ -11,9 +11,17 @@ export const WayScroll = () => {
   return (
     <div className="pointer-events-none sticky bottom-0 z-50 w-screen overflow-hidden pt-10">
       <motion.div
-        className="absolute bottom-0 mx-auto h-16 w-[3000px] bg-[url('/images/bg/runway.png')] bg-right bg-repeat-x [background-size:auto_100%] [filter:blur(10px)] md:h-20"
+        className="my-drop-shadow absolute bottom-0 mx-auto mt-10 h-32 w-[3000px] bg-[url('/images/bg/runway.png')] bg-right-bottom bg-repeat-x [background-size:auto_50%] md:h-40"
         style={{ x: runwayX }}
       />
+      <motion.div
+        className="absolute bottom-0 mx-auto mt-10 h-32 w-[3000px] bg-[url('/images/bg/runway.png')] bg-right-bottom bg-repeat-x [background-size:auto_50%] [filter:blur(5px)] md:h-40"
+        style={{ x: runwayX }}
+      />
+      {/* <motion.div
+        className="absolute bottom-0 mx-auto h-16 w-[3000px] bg-[url('/images/bg/runway.png')] bg-right bg-repeat-x [background-size:auto_100%] [filter:blur(5px)] md:h-20"
+        style={{ x: runwayX }}
+      /> */}
       {/* 底部漸層背景 */}
       {/* <div className="background-blur absolute bottom-0 left-0 h-28 w-full bg-blue-200/50 md:h-32" /> */}
       {/* 跑道背景 */}
