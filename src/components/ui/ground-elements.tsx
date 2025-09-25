cb378258fbf77714fbb66250c7285968d0ad42fa
z01241 <z01241@lawbroker.com.tw>
2025-09-26 01:36:51 +0800

    feat: 更新圖片樣式

--- src/components/ui/ground-elements.tsx
+++ src/components/ui/ground-elements.tsx
@@ -7,6 +7,7 @@ import ground from '@/assets/images/element/ground.png';
 import ice from '@/assets/images/element/ice.png';
 import grass from '@/assets/images/element/grass.png';
 import grass2 from '@/assets/images/element/grass2.png';
+import water from '@/assets/images/element/water.png';
 
 export function GroundElements() {
   return (
@@ -49,16 +50,28 @@ export function GroundElements() {
           <Wolf className="absolute bottom-10 left-5 w-[4.5rem] md:w-20" />
           <Princess className="absolute bottom-10 right-20 w-20 md:w-24 md:right-28" />
           <Hunter className="absolute bottom-10 right-5 w-16 md:w-20" />
-          <div className="absolute bottom-48 left-0 h-10 w-[35vw] bg-[url('/images/element/ground.png')] bg-right bg-repeat-x [background-size:auto_100%]"></div>
+          <div className="absolute bottom-48 left-0 h-10 w-[35vw] bg-right bg-repeat-x [background-size:auto_100%]"
+          style={{
+            backgroundImage: `url(${ground.src})`,
+          }}
+          ></div>
         </div>
       </div>
 
       {/* Middle water section */}
-      <div className="hidden h-10 w-[30%] bg-[url('/images/element/water.png')] [background-size:auto_100%] lg:block"></div>
+      <div className="hidden h-10 w-[30%] [background-size:auto_100%] lg:block"
+        style={{
+          backgroundImage: `url(${water.src})`,
+        }}
+      ></div>
 
       {/* Right section */}
       <div className="relative hidden h-full w-[25%] lg:block">
-        <div className="absolute bottom-0 left-0 h-16 w-full bg-[url('/images/element/ground.png')] bg-left bg-repeat-x [background-size:auto_100%]"></div>
+        <div className="absolute bottom-0 left-0 h-16 w-full bg-left bg-repeat-x [background-size:auto_100%]"
+          style={{
+            backgroundImage: `url(${ground.src})`,
+          }}
+        ></div>
         <Gold className="absolute bottom-24 left-3 w-12 gold-rotate-3d" />
         <Gold className="absolute bottom-24 left-20 w-12 gold-rotate-3d" />
         <Hunter className="absolute bottom-16 left-40 w-24" />
