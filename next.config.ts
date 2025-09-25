6d66a7aeb58085b18723793da00665e22923ec8d
z01241 <z01241@lawbroker.com.tw>
2025-09-26 03:34:09 +0800

    feat: test netlify

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // output: 'export',
+  output: process.env.EXPORT_MODE ? 'export' : 'standalone',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   webpack: (config) => {
